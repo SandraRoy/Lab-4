759c75ce17953253306f4951d8e51e2b89f92690
Sandra Roy <Sandra.D.Roy-1@ou.edu>
2019-02-08 20:34:30 -0600

    Equipment Test completed

--- EquipmentTest.java
+++ EquipmentTest.java
@@ -6,6 +6,12 @@ public class EquipmentTest
 		Assert.assertTrue(equip.getName().equals("Batarang"));
 		Assert.assertTrue(equip.getDescription().equals("bat-shaped boomerang"));
 		Assert.assertEquals(5,equip.getCount());
+		Assert.assertEquals(3.22, equip.getTotalWeight(),0.01);
+		Assert.assertEquals(700.06, equip.getTotalPrice(),0.01);
+		
+	}
+	public void Tst() throws AssertException
+	{
 		
 	}
 }
