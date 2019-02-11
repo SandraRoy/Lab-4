179bfa98b413a582415d3703ab3c2eeec3fc70e3
Sandra Roy <Sandra.D.Roy-1@ou.edu>
2019-02-10 20:15:55 -0600

    EquipmentTest corrections

--- EquipmentTest.java
+++ EquipmentTest.java
@@ -7,7 +7,7 @@ public class EquipmentTest
 		Assert.assertTrue(equip.getDescription().equals("bat-shaped boomerang"));
 		Assert.assertEquals(5,equip.getCount());
 		Assert.assertEquals(3.22, equip.getTotalWeight(),0.01);
-		Assert.assertTrue(700.06, equip.getTotalPrice(),0.01);
+		Assert.assertEquals(700.06, equip.getTotalPrice(),0.01);
 		
 	}
 	public void Test() throws AssertException
